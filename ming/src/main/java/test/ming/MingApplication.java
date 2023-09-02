d10552af747fcfc2bc15ab1ca60a024c742177c0
JoeGukSeon <zipcks1381@gmail.com>
2023-09-03 03:02:53 +0900

    :init: 프로젝트 초기화 - 빌드 도구 설정 - 의존성 라이브러리 추가

---
+++ ming/src/main/java/test/ming/MingApplication.java
@@ -0,0 +1,13 @@
+package test.ming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MingApplication.class, args);
+    }
+
+}
