8d0895f4ab388c0c0a77494cc0a1eff4d976453b
JinuCheon <woojin8787@gmail.com>
2023-09-02 21:54:57 +0900

    feat: response / response 스팩 정의

--- src/test/java/com/example/demo/PageViewTest.java
+++ src/test/java/com/example/demo/PageViewTest.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 //@SpringBootTest
@@ -13,7 +15,7 @@ class PageViewTest {
     @Test
     void testPageView() {
         //given
-        final long pageId = 1L;
+        final String pageId = "dummyuuid1234";
         final PageViewRequest request = new PageViewRequest(pageId);
 
         //when
@@ -21,5 +23,20 @@ class PageViewTest {
 
         //then
         assertThat(response.getId()).isEqualTo(pageId);
+    }
+
+    private record PageViewRequest(String pageId) {
+    }
+
+    private record PageResponse(
+            String id,
+            String title,
+            String content,
+            List<SummaryPageInfo> subPages,
+            List<SummaryPageInfo> breadcrumbs
+    ) {
+    }
+
+    private record SummaryPageInfo(String id, String title) {
     }
 }
