8a76481ac01f90e363a2bef6026292af06f79d61
rhdqors <rhdqors@gmail.com>
2023-09-04 07:10:48 +0900

    #4 feat: 구현 로직
    실제 Http 요청시 Controller 필요,
    입력받은 페이지 id로 데이터 조회

---
+++ src/main/java/com/example/wantednotion/service/PageService.java
@@ -0,0 +1,45 @@
+package com.example.wantednotion.service;
+
+import com.example.wantednotion.domain.Page;
+import com.example.wantednotion.domain.SubPage;
+import com.example.wantednotion.dto.PageResponseDto;
+import com.example.wantednotion.repository.PageRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PageService {
+
+    private final PageRepository pageRepository;
+
+    public PageResponseDto findPageById(String id) {
+        Page page = pageRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("아이디로 조회 실패"));
+
+        List<SubPage> subPages = pageRepository.findAllByParentId(id);
+        List<SubPage> breadcrumbs =getBreadcrumbs(id);
+
+        return new PageResponseDto(page, subPages, breadcrumbs);
+    }
+
+    public List<SubPage> getBreadcrumbs(String id) {
+        List<SubPage> breadcrumbs = new ArrayList<>();
+
+        Page page = pageRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("아이디로 조회 실패"));
+        while(id != null) {
+            if(page != null) {
+                breadcrumbs.add(0, new SubPage(page.getId(), page.getTitle()));
+                id = page.getParentId();
+            } else {
+                break;
+            }
+        }
+        return breadcrumbs;
+    }
+
+}// class
